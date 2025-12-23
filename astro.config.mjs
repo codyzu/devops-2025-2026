ff49248b93ecad069538f6399b0f004838e7781f
Cody Zuschlag <cody.zuschlag@gmail.com>
2025-12-23 15:25:37 +0100

    add: counters to lesson h2 and h3 + expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/vite';
-
 import mdx from '@astrojs/mdx';
+import expressiveCode from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 // https://astro.build/config
 export default defineConfig({
     // Enable React to support React JSX components.
-    integrations: [mdx(), react()],
+    integrations: [
+        expressiveCode({
+            plugins: [pluginLineNumbers()],
+        }),
+        mdx(), 
+        react()],
     vite: {
         plugins: [UnoCSS()],
     },
